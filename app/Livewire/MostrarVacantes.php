307f259f682ca267643f06491f1dc9ffa79f4589
Juan Herrera <juanalbertoherrera99@gmail.com>
2024-06-17 03:46:45 +0200

    comited
    livewire events

--- app/Livewire/MostrarVacantes.php
+++ app/Livewire/MostrarVacantes.php
@@ -7,14 +7,20 @@ use Livewire\Component;
 
 class MostrarVacantes extends Component
 {
+    protected $listeners = ['prueba'];
+
+    public function prueba()
+    {
+        dd('desde prueba...');
+    }
+
     public function render()
     {
         //solo recupera vacantes asociadas al usuario con auth()->user()->id
         $vacantes = Vacante::where('user_id', auth()->user()->id)->paginate(10);
-        
+
         return view('livewire.mostrar-vacantes', [
             'vacantes' => $vacantes
         ]);
-
     }
 }
--- resources/views/livewire/mostrar-vacantes.blade.php
+++ resources/views/livewire/mostrar-vacantes.blade.php
@@ -1,35 +1,31 @@
-<div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg ">
-    @forelse ($vacantes as $vacante)
-        <div class="p-6  text-gray-100 dark:bg-gray-800 border-b border-gray-200 md:flex md:justify-between md:items-center">
-            <div class="leading-10">
+<!-- mostrar-vacantes.blade.php -->
 
-                <a href="#" class="text-xl font-bold">
-                    {{ $vacante->titulo }}
-                </a>
-                <p class="text-sm text-gray-600 font-bold">{{$vacante->empresa}}</p>
-                <p class="text-sm text-gray-500">Último día: {{$vacante->ultimo_dia}}</p>
+<div>
+    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+        @forelse ($vacantes as $vacante)
+            <div class="p-6 text-gray-100 dark:bg-gray-800 border-b border-gray-200 md:flex md:justify-between md:items-center">
+                <div class="leading-10">
+                    <a href="#" class="text-xl font-bold">{{ $vacante->titulo }}</a>
+                    <p class="text-sm text-gray-600 font-bold">{{ $vacante->empresa }}</p>
+                    <p class="text-sm text-gray-500">Último día: {{ $vacante->ultimo_dia }}</p>
+                </div>
+
+                <div class="flex flex-col md:flex-row items-stretch gap-3 mt-5 md:mt-0">
+                    <a href="#" class="bg-gray-500 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">Candidatos</a>
+                    <a href="{{ route('vacantes.edit', $vacante->id) }}" class="bg-blue-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">Editar</a>
+                    <button wire:click="$emit('prueba')" class="bg-red-600 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">Eliminar</button>
+                </div>
             </div>
+        @empty
+            <p class="p-3 text-center text-sm text-gray-600">No hay vacantes que mostrar</p>
+        @endforelse
 
-            <div class="flex flex-col md:flex-row items-stretch gap-3 mt-5 md:mt-0">
-
-                <a href="#" class="bg-gray-500 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">Candidatos</a>
-                <a href="{{route ('vacantes.edit', $vacante->id)}}" class="bg-blue-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">Editar</a>
-                <a href="#" class="bg-red-600 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">Eliminar</a>
-
-            </div>
-
+        <div class="mt-10">
+            {{ $vacantes->links() }}
         </div>
-
-    @empty
-        
-    <p class="p-3 text-center text-sm text-gray-600">No hay vacantes que mostrar</p>
-
-    @endforelse
-
-    <div class="mt-10">
-        {{ $vacantes->links() }}
     </div>
-    
 </div>
 
-
+@push('scripts')
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+@endpush
